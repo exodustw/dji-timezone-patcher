5c0650ec8767334c8b376366ad1f40d2d9eb90e8
exodustw <windows44444444@gmail.com>
2026-04-04 19:36:44 +0800

    now supporting jpg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from glob import glob
+from datetime import datetime, timedelta
 import subprocess
 import argparse
 import sys
@@ -43,15 +44,48 @@ def has_createdate(filepath):
     """檢查是否有 QuickTime:CreateDate"""
     return has_description(filepath, "-CreateDate")
 
-def update_exif_timezone(filepath, new_tz, old_tz=0, large_file_support=False):
+def get_createdate(filepath):
+    """取得影片內部的 CreateDate，若無則回傳 None"""
+    cmd = ["exiftool", "-s3", "-CreateDate", filepath]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    out = result.stdout.strip()
+
+    if out:
+        return out
+    return None
+
+def get_current_exif_tz(filepath):
+    """
+    取得檔案目前的時區標記 (例如 +08:00)
+    """
+    # 嘗試取得照片的 EXIF 時區或影片的 Keys 時區
+
+    cmd = [
+        "exiftool", "-s3", 
+        "-OffsetTimeOriginal", # 照片優先
+        "-CreationDate",       # 影片(Keys)
+        filepath
+    ]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    output = result.stdout.strip()
+    
+    # 使用正則表達式抓取尾部的 +HH:MM 或 -HH:MM
+    match = re.search(r'([+-])(\d{2}):(\d{2})$', output)
+    
+    if match:
+        sign = 1 if match.group(1) == '+' else -1
+        hours = int(match.group(2)) * sign
+        minutes = int(match.group(3)) * sign
+        return (hours, minutes)
+    
+    return None
+
+def update_exif_timezone(filepath, new_tz, default_tz=8, large_file_support=False, debug=False):
     """
     file_path: 影片路徑
     new_tz: 目標時區 (例如預期是 +9，就輸入 9)
     """
-    shift = new_tz - old_tz
-    
-    # 建立偏移字串，例如 "+1" 或 "-5"
-    shift_str = f"{shift:+d}"
+    file_ext = os.path.basename(filepath).split('.')[-1].lower()
     
     # 時區標籤字串，例如 "+09:00"
     tz_str = f"{new_tz:+03d}:00"
@@ -72,28 +106,67 @@ def update_exif_timezone(filepath, new_tz, old_tz=0, large_file_support=False):
     if large_file_support:
         cmd.extend(["-api", "LargeFileSupport=1"])
     
-    if has_createdate(filepath):
+    if file_ext == 'mp4':
+        shift = new_tz - 0
+        shift_str = f"{shift:+d}" # 建立偏移字串，例如 "+1" 或 "-5"
+
+        create_date = get_createdate(filepath)
+        if create_date is None:
+            create_date = get_time_from_filename(os.path.basename(filepath))
+
+            if not create_date:
+                print(f"Failed (No time info from filename)")
+                return False
+            
+            create_date_ts = datetime.strptime(create_date, "%Y:%m:%d %H:%M:%S") + timedelta(hours=-default_tz)
+            create_date = create_date_ts.strftime("%Y:%m:%d %H:%M:%S")
+        else:
+            create_date_ts = datetime.strptime(create_date, "%Y:%m:%d %H:%M:%S")
+        
+        local_date_ts = create_date_ts + timedelta(hours=shift)
+        local_date = local_date_ts.strftime("%Y:%m:%d %H:%M:%S")
+
         cmd.extend([
-            "-XMP:description<CreateDate",
-            f"-globalTimeShift", f"{shift_str}",
-            f"-Keys:CreationDate<${{CreateDate}}{tz_str}",
-            # f"-UserData:DateTimeOriginal<${{CreateDate}}{tz_str}",
+            "-globalTimeShift", shift_str,
+            f"-XMP:description={create_date}",
+            f"-Keys:CreationDate={local_date}{tz_str}",
+            filepath
+        ])
+        
+    elif file_ext == 'jpg':
+        exif_tz = get_current_exif_tz(filepath)
+        
+        if exif_tz is not None:
+            old_tz = exif_tz[0] + exif_tz[1] / 60
+        else:
+            old_tz = default_tz
+        
+        shift = new_tz - old_tz
+
+        create_date = get_createdate(filepath)
+        if create_date is None:
+            print(f"Failed (No time info from exif)")
+            return False
+
+        create_date_ts = datetime.strptime(create_date, "%Y:%m:%d %H:%M:%S")
+        local_date_ts = create_date_ts + timedelta(hours=shift)
+        local_date = local_date_ts.strftime("%Y:%m:%d %H:%M:%S")
+
+        cmd.extend([
+            f"-AllDates={local_date}{tz_str}",
+            f"-OffsetTime*={tz_str}",                       # 修正 EXIF 時區標籤
+            f"-XMP-xmp:CreateDate={local_date}{tz_str}", # 修正 XMP 標籤
+            f"-XMP-xmp:ModifyDate={local_date}{tz_str}",
+            # f"-Keys:CreationDate={local_date}{tz_str}",
+            # f"-XMP:description<${{CreateDate}}",
             filepath
         ])
     else:
-        filetime = get_time_from_filename(os.path.basename(filepath))
-        if filetime:
-            cmd.extend([
-                f"-CreateDate={filetime}",
-                # f"-XMP:Description={filetime}",
-                "-XMP:description<CreateDate",
-                "-globalTimeShift", shift_str,
-                f"-Keys:CreationDate<${{CreateDate}}{tz_str}",
-                filepath
-            ])
-        else:
-            print(f"Failed (No time info from filename)")
-            return False
+        print(f"Failed (Unsupport file extension .{file_ext})")
+        return False
+
+    if debug:
+        print('command', ' '.join(cmd))
 
     try:
         result = subprocess.run(cmd, check=True, capture_output=True, text=True)
@@ -112,19 +185,24 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", help="path")
     # type=int 自動檢查是否為整數; choices 限制輸入範圍
-    parser.add_argument("tz", type=int, choices=range(-12, 15), help="timezone")
-    parser.add_argument("--overwrite", action="store_true", help="overwrite")
-    parser.add_argument("--large-file", action="store_true", help="large file support")
+    parser.add_argument("newtz", type=int, choices=range(-12, 15), help="correct timezone")
+    parser.add_argument("-t", "--default-tz", type=int, default=8, choices=range(-12, 15), help="default timezone if not presented in file")
+    parser.add_argument("-f", "--overwrite", action="store_true", help="overwrite")
+    parser.add_argument("-l", "--large-file", action="store_true", help="large file support")
+    parser.add_argument("-d", "--debug", action="store_true", help="debug flag")
+    parser.add_argument("-y", "--yes", action="store_true", help="disable safety lock")
 
     args = parser.parse_args()
 
     target = args.dir
-    new_timezone = args.tz
+    new_timezone = args.newtz
+    default_timezone = args.default_tz
 
-    yesno = input(f"Are you sure to rename? (y/n): ")
-    if yesno.strip().lower() != 'y':
-        print("Canceled.")
-        sys.exit(4)
+    if not args.yes:
+        yesno = input(f"Are you sure to rename? (y/n): ")
+        if yesno.strip().lower() != 'y':
+            print("Canceled.")
+            sys.exit(4)
 
     for filename in sorted(glob(target)):
         if os.path.isfile(filename):
@@ -134,6 +212,6 @@ if __name__ == "__main__":
                 print(f"Skipped.")
                 continue
 
-            if not update_exif_timezone(filename, new_timezone, large_file_support=args.large_file):
+            if not update_exif_timezone(filename, new_timezone, default_timezone, large_file_support=args.large_file, debug=args.debug):
                 break
             print(f"Modified.")
